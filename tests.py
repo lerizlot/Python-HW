fceeb0a478b15f02d1c1af0a7814a898dba95a55
Valeriia <lerizlot@icloud.com>
2024-05-27 08:43:15 +0400

    second commit

---
+++ tests.py
@@ -0,0 +1,11 @@
+print('sMaRtPhOnE'.lower())
+print('sMaRtPhOnE'.upper())
+print('sMaRtPhOnE'.capitalize())
+
+item = "telephone"
+print(item.find("n"))
+
+
+word = 'vehicle'
+print(word.find('r'))
+
